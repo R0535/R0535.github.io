96585bfe764d1448e17a5a5e3d93cd2ed986bcb4
José Hernandez <jghr053@gmail.com>
2021-12-05 00:25:18 -0600

    automated

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '📸';
+};
+
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Casa_Pepe',
+           location: {
+               lat: 19.710703,
+               lng: -101.179357 // Casa Pepe,
+           }
+       },
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', 'url(assets/Grimm/untitled5.gltf)');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '20 20 20');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
