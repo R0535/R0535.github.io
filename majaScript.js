e3160012c971a4530f99f1763c0c32b88ad217a3
R0535 <jghr053@gmail.com>
2022-09-26 15:37:47 -0500

    cursor

--- majaScript.js
+++ majaScript.js
@@ -4,6 +4,22 @@ var jsonObj = {};
 window.onload = () => {
   staticLoadImages();
 
+  document.querySelector("#nextSphere").addEventListener("click", (e) => {
+    if (init < jsonObj.length) {
+      init++;
+      console.log(init);
+      renderSky(jsonObj, init);
+    }
+  });
+  document.querySelector("#prevSphere").addEventListener("click", (e) => {
+    if (init > 0) {
+      init--;
+      console.log(init);
+      renderSky(jsonObj, init);
+    }
+  });
+
+  /*
   document.querySelector("#next").addEventListener("click", (e) => {
     if (init < jsonObj.length) {
       init++;
@@ -17,13 +33,8 @@ window.onload = () => {
       console.log(init);
       renderSky(jsonObj, init);
     }
-  });
-
+  });*/
 };
-
-
-
-
 
 function staticLoadImages() {
   fetch("images.json")
@@ -43,12 +54,30 @@ function renderSky(places, index) {
   let initial = places[index];
   console.log(initial);
 
-
+  /*Delete all old elements*/
   let old_sky = document.querySelector("a-sky");
-  if(old_sky!=null) {
-    old_sky.remove
+  if (old_sky != null) {
+    old_sky.remove;
+  }
+  //delete text
+  let old_text = document.querySelector("#modularText");
+
+  if (old_text != null) {
+    old_text.setAttribute("value", initial.desc);
+
+  } else {
+    //Text
+    const entity = document.createElement("a-text");
+    entity.id = "modularText";
+    //entity.setAttribute("id", "modularText");
+    entity.setAttribute("value", initial.desc);
+    entity.setAttribute("width", "3");
+    entity.setAttribute("position", "0 .25 -3");
+    scene.appendChild(entity);
   }
 
+  /*Create new elements*/
+  //SKY
   const sky = document.createElement("a-sky");
   sky.setAttribute("src", `${initial.image_id}`);
 
