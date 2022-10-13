e0d4eb6637a3be8e37f9b46d75f356455335c497
José Hernandez <jghr053@gmail.com>
2022-10-13 14:42:21 -0500

    a

--- majaScript.js
+++ majaScript.js
@@ -1,4 +1,4 @@
-var init = 0;
+var init = 20;
 var jsonObj = {};
 var imagesLoading = "imagesAcueducto.json"
 
@@ -7,6 +7,7 @@ var imagesLoading = "imagesAcueducto.json"
 window.onload = () => {
   staticLoadImages(imagesLoading);
 
+
   document.querySelector("#nextSphere").addEventListener("click", (e) => {
     if (init < jsonObj.length) {
       init++;
@@ -14,6 +15,7 @@ window.onload = () => {
       renderSky(jsonObj, init);
     }
   });
+
   document.querySelector("#prevSphere").addEventListener("click", (e) => {
     if (init > 0) {
       init--;
@@ -21,22 +23,6 @@ window.onload = () => {
       renderSky(jsonObj, init);
     }
   });
-
-  /*
-  document.querySelector("#next").addEventListener("click", (e) => {
-    if (init < jsonObj.length) {
-      init++;
-      console.log(init);
-      renderSky(jsonObj, init);
-    }
-  });
-  document.querySelector("#prev").addEventListener("click", (e) => {
-    if (init > 0) {
-      init--;
-      console.log(init);
-      renderSky(jsonObj, init);
-    }
-  });*/
 };
 
 function staticLoadImages(json="") {
@@ -49,40 +35,72 @@ function staticLoadImages(json="") {
 }
 
 function renderSky(places, index) {
-  /***
-   * Los eventos
-   */
 
-  let scene = document.querySelector("a-scene");
   let initial = places[index];
-  console.log(initial);
 
-  /*Delete all old elements*/
-  let old_sky = document.querySelector("a-sky");
-  if (old_sky != null) {
-    old_sky.remove;
+  setNavigatorsLineal(index, places.length)
+
+  refreshSky(initial.img_src)//swap images
+  replaceDescription(initial.desc)//swap text
+
+}
+
+
+//Decide si poner una bola inicial o final para navegar
+function setNavigatorsLineal(index, limit){
+  let enablePrev = true
+  let enableNext = true
+  if(index == 0){
+    enablePrev = false
   }
-  //delete text
+
+  if(index == limit-1){
+    enableNext = false
+  }
+
+    //Prev Sphere
+  document.querySelector("#prevSphere").setAttribute("visible", enablePrev)
+  document.querySelector("#prevText").setAttribute("visible", enablePrev)
+  document.querySelector("#prevTextShadow").setAttribute("visible", enablePrev)
+    //Next Sphere
+  document.querySelector("#nextSphere").setAttribute("visible", enableNext)
+  document.querySelector("#nextText").setAttribute("visible", enableNext)
+  document.querySelector("#nextTextShadow").setAttribute("visible", enableNext)
+}
+
+//Elimina la esfera de textura que rodea
+function refreshSky(image_src){
+
+
+  let old_sky = document.querySelector("a-sky");
+  if (old_sky != null) {//Only first time is null
+    old_sky.setAttribute("src", image_src);
+  }else{
+    const sky = document.createElement("a-sky");
+    sky.setAttribute("src", image_src);
+  
+    let scene = document.querySelector("a-scene");
+    scene.appendChild(sky);
+  }
+}
+
+//Remplaza el contenido de texto de texto de las decripciones
+function replaceDescription(text){
+
+
+
   let old_text = document.querySelector("#modularText");
 
   if (old_text != null) {
-    old_text.setAttribute("value", initial.desc);
-
+    old_text.setAttribute("value", text);
   } else {
-    //Text
     const entity = document.createElement("a-text");
     entity.id = "modularText";
-    //entity.setAttribute("id", "modularText");
-    entity.setAttribute("value", initial.desc);
+    entity.setAttribute("value", text);
     entity.setAttribute("width", "3");
     entity.setAttribute("position", "0 .25 -3");
+    
+    let scene = document.querySelector("a-scene");
     scene.appendChild(entity);
   }
-
-  /*Create new elements*/
-  //SKY
-  const sky = document.createElement("a-sky");
-  sky.setAttribute("src", `${initial.image_id}`);
-
-  scene.appendChild(sky);
 }
